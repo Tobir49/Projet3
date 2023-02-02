2f236d0d69613e5406406826a1d22b18cff56044
Baptiste Ribot <baptisteribot@outlook.fr>
2023-02-02 11:22:12 +0100

    test de fonction pour affichage projets

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,34 +1,48 @@
 //Récup des figures depuis l'API
 async function afficherTravail() {
-    const element = await fetch('http://localhost:5678/api/works');
+    const reponse = await fetch('http://localhost:5678/api/works');
+    const json = await reponse.json();
+    console.log(json);
 }
 
-function recupererFigure(element) {
-    for (let i = 0; i < element.length; i++) {
+afficherTravail();
 
-        const work = element[i];
-        // Classe qui comporte toutes les figures
-        const classeGalerry = document.querySelector(".gallery");
-        // Balise qui contient les images et le texte (<figure>)
-        const totalFigure = document.createElement("figure");
-        // Balises présentes dans une <figure>
-        const imageFigure = document.createElement("img");
-        imageFigure.src = work.imageUrl;
-        const nomFigure = document.createElement("figcaption");
-        nomFigure.innerText = work.title;
-        // Rattachement des balises à un parent (pour être affiché sur la page)
-        classeGalerry.appendChild(totalFigure);
-        totalFigure.appendChild(imageFigure);
-        totalFigure.appendChild(nomFigure);
 
+//Cette fonction sert à créer les figures présentes de base dans le HTML
+function creerFigure(works) {
+    const element = document.querySelector('figure');
+
+    // 1. On importe l'image
+    const imageFigure = document.createElement("img");
+    imageFigure.src = works.imageUrl;
+
+    // 2. On importe le texte
+    const texteFigure = document.createElement("figcaption");
+    texteFigure.src = works.title;
+
+    return element;
+}
+
+
+//Création d'une fonction pour afficher les figures (Image + Texte) sur la page
+function afficherFigure(figure) {
+    gallery = document.getElementsByClassName(".gallery");
+    gallery.appendChild(element);
+}
+
+// La fonction comporte une boucle, pour afficher tous les travaux 
+function chargementEtAffichageTravaux() {
+
+    // 1. Récupération des travaux sur l'API
+    const tableauWork = fetch("http://localhost:5678/api/works");
+
+    // 2. Créer les figure en parcourant le tableau
+    for (i = 0; i < tableauWork.length; i++) {
+        figure = creerFigure(tableauWork[i]);
+        afficherFigure(figure);
     }
 }
 
-recupererFigure(element);
 
-//Création d'une fonction pour afficher les figures (Image + Texte) sur la page
-
-function afficherFigure(figure) {
-    gallery = document.getElementsByClassName(".gallery");
-    gallery.appendChild(figure);
-}
+//On appelle la fonction
+chargementEtAffichageTravaux();
