2d3be305b1978e867a3bd986603e956607b93f90
Baptiste Ribot <baptisteribot@outlook.fr>
2023-02-06 15:09:06 +0100

    ajout des buttons et du css pour les filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,7 @@
-//Récup du tableau des travaux (works) depuis l'API grâce à une fonction asynchrone
+//Récup du tableau des travaux depuis l'API grâce à une fonction asynchrone
 async function recupererTravail() {
-    const reponse = await fetch('http://localhost:5678/api/works'); // await pour aller avec le async
+    const reponse = await fetch('http://localhost:5678/api/works');
     const json = await reponse.json();
-    //Vérifier que le tableau est bien importé (via la console dans le navigateur)
     return json;
 }
 
@@ -13,7 +12,7 @@ async function recupererTravail() {
 function creerFigure(work) {
     // 1. On crée la constante "element" qui est lié aux balises <figure> du HTML
     const element = document.createElement('figure');
-    // 2. On crée une balise image
+    // 2. On crée une balise image et on lui ajoute setAttribute sinon elles ne se chargent pas
     const imageFigure = document.createElement("img");
     imageFigure.setAttribute('crossorigin', 'anonymous');
     // On va chercher cet élément dans l'API
@@ -21,15 +20,17 @@ function creerFigure(work) {
 
     // 3. On importe le texte
     const texteFigure = document.createElement("figcaption");
+    // On va chercher cet élément dans l'API
     texteFigure.innerText = work.title;
 
+    //Liaison des éléments à un parent
     element.appendChild(imageFigure);
     element.appendChild(texteFigure);
 
     return element;
 }
 
-//Création d'une fonction pour créer des liens parents-enfants et donc afficher ces éléments
+//Création d'une fonction pour créer le parent des éléments de la fonction creerFigure
 function afficherFigure(figure) {
     // Tout ce qui découle de la classe gallery du HTML
     gallery = document.getElementsByClassName("gallery")[0];
@@ -55,3 +56,6 @@ function chargementEtAffichageTravaux() {
 
 //On appelle la fonction
 chargementEtAffichageTravaux();
+
+
+// Maintenant que les travaux sont affichés via l'API, on fait en sorte de trier les projets par catégorie.
