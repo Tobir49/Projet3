a66bc83f235418a0cb6dd0b168fe19ab4d88b6b9
Baptiste Ribot <baptisteribot@outlook.fr>
2023-02-22 10:45:20 +0100

    fonction connexion finie et suppression page admin

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,14 +2,14 @@
 
 /* Pour se connecter il faut :
     
-1. Créer une fonction pour rediriger sur la page HTML (utilisée si la connexion est réussie*/
+1. Créer une fonction pour rediriger sur la page HTML (utilisée si la connexion est réussie)*/
 
 function connexionReussie() {
-    window.location.href = "../index.html";
+    window.location.href = "./index.html";
 };
 
-/* Ensuite il faut une autre fonction qui sera elle aussi appelée plus tard
-2. Fonction qui génère un message d'erreur si la connexion échoue*/
+/* Une autre fonction qui sera aussi appelée plus tard :
+2. Générer un message d'erreur si la connexion échoue*/
 
 function connexionEchouee() {
     const erreur = document.querySelector(".erreur");
@@ -20,26 +20,48 @@ function connexionEchouee() {
 }
 
 /* Une fois ces fonctions créées :
-3. Fonction qui permet de se connecter (utilisation des fonctions précédentes)*/
+3. Fonction qui permet de se connecter (+ appel des fonctions précédentes)*/
 
-async function fonctionConnexion() {
+function fonctionConnexion() {
     const formulaire = document.querySelector(".formulaire-connexion");
-    console.log(formulaire);
 
-    // Accès aux inputs du <form> dans le HTML grâce à getElementById
-    const emailFormulaire = document.getElementById("email").value; // Le .value sert à récupérer la valeur écrite dans cet input
-    const passwordFormulaire = document.getElementById("password").value;
+    formulaire.addEventListener("submit", async function(event) {
+        event.preventDefault();
 
-    // On crée cette constante qui nous sera utile pour l'appel à fetch
-    const elementsFormulaire = {
-        email: emailFormulaire,
-        password: passwordFormulaire,
-    };
+        // 1. Accès aux inputs du <form> dans le HTML grâce à getElementById
+        const emailFormulaire = document.getElementById("email").value; // Le .value sert à récupérer la valeur écrite dans cet input
+        const passwordFormulaire = document.getElementById("password").value;
 
-    // Appel à fetch
-    let response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(elementsFormulaire) // On passe les éléments du formulaire au format JSON
+        // 2. On crée cette constante qui nous sera utile pour l'appel à fetch
+        const elementsFormulaire = {
+            email: emailFormulaire,
+            password: passwordFormulaire,
+        };
+
+        // 3. Appel à fetch
+        let response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(elementsFormulaire) // On passe les éléments du formulaire au format JSON
+        });
+
+        // 4. On récupère le token grâce à la réponse du fetch
+        let reponseFetch = await response.json();
+        let token = reponseFetch.token;
+        console.log(token);
+
+        // 5. On appelle les fonctions créées précédemment :
+
+        // 5.A : Si la connexion est réussie : 
+        if (response.status === 200) {
+            // On stocke le token dans le localStorage pour le récupérer plus tard
+            window.localStorage.setItem("token", token);
+            connexionReussie();
+        } /* 5.B Si la connexion échoue : */
+        else if (response.status === 401 || 404) {
+            connexionEchouee();
+        }
     });
-}
+};
+
+fonctionConnexion();
