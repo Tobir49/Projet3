544a901712138bf99696eed279ed30670f72bed0
Baptiste Ribot <baptisteribot@outlook.fr>
2023-02-22 09:56:39 +0100

    découpage de la fonction connexion

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,5 +1,46 @@
 //\\\\\\\\\\\\\\\\\Se connecter/////////////////////\\
 
+/* Pour se connecter il faut :
+    
+1. Créer une fonction pour rediriger sur la page HTML (utilisée si la connexion est réussie*/
+
+function connexionReussie() {
+    window.location.href = "../index.html";
+};
+
+/* Ensuite il faut une autre fonction qui sera elle aussi appelée plus tard
+2. Fonction qui génère un message d'erreur si la connexion échoue*/
+
+function connexionEchouee() {
+    const erreur = document.querySelector(".erreur");
+    document.querySelector(".erreur").innerHTML = "";
+    const afficherErreur = document.createElement("p");
+    afficherErreur.innerText = "Erreur dans l’identifiant ou le mot de passe";
+    erreur.appendChild(afficherErreur);
+}
+
+/* Une fois ces fonctions crées :
+3. Fonction qui permet de se connecter (utilisation des fonctions précédentes)*/
+
+
+
+// async function login() {
+//     let user = {
+//         email: document.getElementById("email").value,
+//         password: document.getElementById("password").value,
+//     };
+//     let reponse = await fetch(apiLogin, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8',
+//         },
+//         body: JSON.stringify(user)
+//     });
+//     window.location.href('../index.html')
+// };
+
+// login();
+
 // function check(form) {
 //     if (form.email.value == "sophie.bluel@test.tld" && form.password.value == "S0phie") {
 //         return true;
@@ -8,31 +49,4 @@
 //         return false;
 //     }
 // };
-
-
-// let token = "http://localhost:5678/api/users/login";
-
-// /* if (token) {
-//     rediriger vers la page de base
-//     créer les boutons edit et la bannière
-// } */
-
-
-const apiLogin = "http://localhost:5678/api/users/login";
-
-async function login() {
-    let user = {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
-    };
-    let reponse = await fetch(apiLogin, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(user)
-    });
-    window.location.href('../index.html')
-};
-
-login();
+// check(form);
